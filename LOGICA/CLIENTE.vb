4ab5b7c03bfb4da4c42531b1a9b8e4ad4dda593c
lulalulo <145867789+lulalulo@users.noreply.github.com>
2023-09-23 13:01:28 -0700

    Logica files

---
+++ LOGICA/CLIENTE.vb
@@ -0,0 +1,105 @@
+﻿Public Class CLIENTE
+
+    Public Shared Function Cliente_Select() As DataTable
+
+        Return DATOS.CLIENTE.Cliente_Select
+
+    End Function
+
+    Public Shared Function SUPPLIERS_INSERTAR(
+        ByVal CompanyName As String, ByVal ContactName As String,
+        ByVal ContactTitle As String, ByVal Address As String,
+        ByVal City As String, ByVal Region As String,
+        ByVal PostalCode As String, ByVal Country As String,
+        ByVal Phone As String, ByVal Fax As String,
+        ByVal HomePage As String) As Boolean
+
+        Return DATOS.CLIENTE.SUPPLIERS_INSERTAR(CompanyName, ContactName,
+                                                ContactTitle, Address,
+                                                City, Region,
+                                                PostalCode, Country,
+                                                Phone, Fax, HomePage)
+
+    End Function
+
+    'NUEVA VERSION
+    Public Shared Function SUPPLIERS_INSERTAR2(
+        ByVal CompanyName As String, ByVal ContactName As String,
+        ByVal ContactTitle As String, ByVal Address As String,
+        ByVal City As String, ByVal Region As String,
+        ByVal PostalCode As String, ByVal Country As String,
+        ByVal Phone As String, ByVal Fax As String,
+        ByVal HomePage As String, ByVal SupplierID As Integer) As Boolean
+
+        Return DATOS.CLIENTE.SUPPLIERS_INSERTAR2(CompanyName, ContactName,
+                                                ContactTitle, Address,
+                                                City, Region,
+                                                PostalCode, Country,
+                                                Phone, Fax, HomePage, SupplierID)
+
+
+    End Function
+
+    Public Shared Function SUPPLIERS_UPDATE(
+        ByVal CompanyName As String, ByVal ContactName As String,
+        ByVal ContactTitle As String, ByVal Address As String,
+        ByVal City As String, ByVal Region As String,
+        ByVal PostalCode As String, ByVal Country As String,
+        ByVal Phone As String, ByVal Fax As String,
+        ByVal HomePage As String, ByVal SupplierID As Integer) As Boolean
+
+        Return DATOS.CLIENTE.SUPPLIERS_UPDATE(CompanyName, ContactName,
+                                                ContactTitle, Address,
+                                                City, Region,
+                                                PostalCode, Country,
+                                                Phone, Fax, HomePage, SupplierID)
+
+    End Function
+
+
+    'NUEVA VERSION
+    Public Shared Function SUPPLIERS_UPDATE2(
+        ByVal CompanyName As String, ByVal ContactName As String,
+        ByVal ContactTitle As String, ByVal Address As String,
+        ByVal City As String, ByVal Region As String,
+        ByVal PostalCode As String, ByVal Country As String,
+        ByVal Phone As String, ByVal Fax As String,
+        ByVal HomePage As String, ByVal SupplierID As Integer) As Boolean
+
+        Return DATOS.CLIENTE.SUPPLIERS_UPDATE2(CompanyName, ContactName,
+                                                ContactTitle, Address,
+                                                City, Region,
+                                                PostalCode, Country,
+                                                Phone, Fax, HomePage, SupplierID)
+
+    End Function
+
+    Public Shared Function SUPPLIERS_DELETE(ByVal SupplierID As Integer) As Boolean
+
+        Return DATOS.CLIENTE.SUPPLIERS_DELETE(SupplierID)
+
+    End Function
+
+    Public Shared Function SUPPLIERS_BUSCAR(ByVal Campo As Integer, ByVal Criterio As String) As DataTable
+
+        Return DATOS.CLIENTE.SUPPLIERS_BUSCAR(Campo, Criterio)
+
+    End Function
+
+    Public Shared Function SUPPLIERS_BUSQUEDAVAN(
+       ByVal CompanyName As String, ByVal ContactName As String,
+       ByVal ContactTitle As String, ByVal Address As String,
+       ByVal City As String, ByVal Region As String,
+       ByVal PostalCode As String, ByVal Country As String,
+       ByVal Phone As String, ByVal Fax As String,
+       ByVal HomePage As String, Optional ByVal SupplierID As Integer? = Nothing) As DataTable
+
+        Return DATOS.CLIENTE.SUPPLIERS_BUSQUEDAVAN(CompanyName, ContactName,
+                                                ContactTitle, Address,
+                                                City, Region,
+                                                PostalCode, Country,
+                                                Phone, Fax, HomePage, SupplierID)
+
+    End Function
+
+End Class
---
+++ LOGICA/USUARIO.vb
@@ -0,0 +1,10 @@
+﻿Public Class USUARIO
+
+    Public Function USUARIO_LOGIN(ByVal Nombre As String, ByVal Contraseña As String) As Boolean
+
+        Dim ousuario As New DATOS.USUARIO
+        Return ousuario.USUARIO_LOGIN(Nombre, Contraseña)
+
+    End Function
+
+End Class
